85f0f51d0ee60961afdd98fe2c000b226cd7651b
Tanvir Ahmmed <139853229+tanvirahmmed2@users.noreply.github.com>
2025-09-16 22:35:14 +0600

    Update Sidebar.jsx

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -9,7 +9,7 @@ import { CreateContext } from '../component/Context/CreateContext'
 const Sidebar = () => {
     const { sidebar, setSidebar } = useContext(CreateContext)
     return (
-    <section className={`w-[200px] h-screen flex flex-col items-center justify-around py- 16 ${sidebar? 'translate-x-0': '-translate-x-full'} transform transition-transform duration-500 ease-in-out fixed top-0 left-0 backdrop-blur-2xl z-50`}>
+    <section className={`w-[200px] h-screen flex flex-col items-center justify-around py-16 ${sidebar? 'translate-x-0': '-translate-x-full'} transform transition-transform duration-500 ease-in-out fixed top-0 left-0 backdrop-blur-2xl z-50`}>
             <div className={`w-auto flex pb-4 flex-col items-start justify-center`}>
                 <a href="/" className='text-3xl font-semibold my-7'>Disibin</a>
                 <Link className=' flex items-center justify-center p-2' onClick={() => setSidebar(!sidebar)} to="/">Home</Link>
