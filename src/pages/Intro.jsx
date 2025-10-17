43a45ec5695a1a0d021c3d836b2b105e2820140d
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-18 00:30:44 +0600

    update

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -8,9 +8,8 @@ import { CiMail } from 'react-icons/ci'
 
 const Intro = () => {
   return (
-    <section className="w-full  h-auto p-4">
-      <div className="w-full h-auto flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-2">
-        <div className="w-full flex flex-col p-4 items-start justify-center gap-3 text-lg">
+    <section className="w-full  h-auto p-4 items-center justify-center flex">
+        <div className="w-full flex flex-col p-4 items-center justify-center gap-3 text-lg">
           <p>Web & Graphics Development Studio</p>
           <p className="text-8xl font-bold">
             <Typewriter
@@ -19,17 +18,17 @@ const Intro = () => {
               delaySpeed={1000}
             />
           </p>
-          <h1 className="text-3xl">
+          <h1 className="text-base sm:text-xl md:text-2xl font-semibold">
             Websites & graphics that grow your business
           </h1>
-          <p>
+          <p className="text-xs lg:text-base text-center">
             We build modern websites and create standout graphics for small
             teams and startups. Clear messaging, clean visuals, and results you
             can measure
           </p>
 
 
-          <div className="w-full flex flex-row items-center justify-start gap-4">
+          <div className="w-full flex flex-row items-center justify-center gap-4">
             <div className={`flex w-32  mt-2 flex-col  group relative group/item2  hover:bg-transparent transition-all duration-500 ease-out origin-left rounded-lg`}>
               <span className='absolute border-l-2 border-indigo-500 inset-0 bg-indigo-500 w-0 group-hover/item2:w-full transition-all duration-500 ease-out origin-left rounded-lg shadow-lg shadow-indigo-600'></span>
               <Link to='/projects' download className='h-8 w-full gap-2  flex items-center justify-center px-4 rounded-lg z-10  font-semibold  text-red-600 hover:text-white transition duration-1000'>Projects</Link>
@@ -57,32 +56,8 @@ const Intro = () => {
 
 
 
-        <div className="w-full rounded-2xl gap-2 m-4 flex flex-col items-center justify-center opacity-100 shadow-sm shadow-indigo-500 p-4 border-2 border-red-500 border-opacity-20 bg-white/5">
-
-          <div className="w-full h-auto flex flex-row items-center justify-center gap-2">
-            <div className="w-1/3 h-[200px] bg-white/5 overflow-hidden cursor-pointer hover:scale-105 opacity-50 hover:opacity-100 transition duration-300 rounded-lg flex flex-col items-center justify-center text-xl gap-2 ">
-              <img src="https://res.cloudinary.com/dz45x9efk/image/upload/v1756875260/webdesign_eeslve.jpg" alt="" className="w-full h-[200px] object-cover " />
-            </div>
-            <div className="w-2/3 h-[200px] bg-emerald-400 overflow-hidden cursor-pointer hover:scale-105 transition opacity-50 hover:opacity-100 duration-300 rounded-lg flex flex-col items-center justify-center text-xl gap-2 ">
-              <img src="https://res.cloudinary.com/dz45x9efk/image/upload/v1756875256/webdev_aphw47.jpg" alt="" className="w-full h-[200px] object-cover " />
-            </div>
-
-          </div>
-
-          <div className="w-full h-auto flex flex-row items-center justify-center gap-2">
-            <div className="w-2/3 h-[200px] overflow-hidden  bg-emerald-400  cursor-pointer hover:scale-105 transition duration-300 opacity-50 hover:opacity-100 rounded-lg flex flex-col items-center justify-center text-xl gap-2 ">
-              <img src="https://res.cloudinary.com/dz45x9efk/image/upload/v1756875259/graphicsdesign_va594g.jpg" alt="grphicsdesignimg" className="w-full h-[200px] object-cover " />
-
-
-            </div>
-            <div className="w-1/3 h-[200px] bg-white/5 cursor-pointer hover:scale-105 transition overflow-hidden duration-300  rounded-lg flex  opacity-50 hover:opacity-100 flex-col items-center justify-center text-xl gap-2 ">
-              <img src="https://res.cloudinary.com/dz45x9efk/image/upload/v1756875259/care_rfsmrw.jpg" alt="" className="w-full h-[200px] object-cover " />
-            </div>
-          </div>
-
-        </div>
-
-      </div>
+      
+      
     </section>
   );
 };
