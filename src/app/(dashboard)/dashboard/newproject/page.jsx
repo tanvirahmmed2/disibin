7e37c6554dca9e568631270b01b49513b7edb406
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-02-05 20:48:28 +0600

    form

--- src/app/(dashboard)/dashboard/newproject/page.jsx
+++ src/app/(dashboard)/dashboard/newproject/page.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 
 const NewProjectPage = () => {
   return (
-    <div className='w-full'>
+    <div className='w-full p-4 flex flex-col items-center justify-center gap-4'>
+      <h1 className='text-center font-semibold text-2xl text-emerald-500'>Add New Project</h1>
       <AddNewProjectForm/>
     </div>
   )
--- src/component/forms/AddNewProjectForm.jsx
+++ src/component/forms/AddNewProjectForm.jsx
@@ -44,7 +44,34 @@ const AddNewProjectForm = () => {
   return (
     <form onSubmit={handleSubmit} className='w-full flex flex-col items-center justify-center gap-4'>
       <div>
-        
+        <label htmlFor="title">Title</label>
+        <input type="text" name='title' id='title' required value={formData.title} onChange={handleChange}/>
+      </div>
+      <div>
+        <div>
+          <label htmlFor="preview">Preview</label>
+          <input type="text" name='preview' id='preview' onChange={handleChange} required value={formData.preview} placeholder='set preview link'/>
+        </div>
+        <div>
+          <label htmlFor="category">Category</label>
+          <input type="text" name='category' id='category' value={formData.category} onChange={handleChange} />
+        </div>
+      </div>
+      <div>
+        <label htmlFor="description">Description</label>
+        <textarea name="description" id="description" required onChange={handleChange} value={formData.description} placeholder='write here'></textarea>
+      </div>
+      <div>
+        <label htmlFor="tags">Tags</label>
+        <input type="text" name='tags' id='tags' required onChange={handleChange} value={formData.tags} placeholder="tags ('||')"/>
+      </div>
+      <div>
+        <label htmlFor="skills">skills</label>
+        <input type="text" name='skills' id='skills' required onChange={handleChange} value={formData.skills} placeholder="skills ('||')"/>
+      </div>
+      <div>
+      <label htmlFor="image">Image *</label>
+      <input type="file" accept='*/image' />
       </div>
 
     </form>
