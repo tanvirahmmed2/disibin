6198df7774322fe69eb6979e7540764cc6dadf57
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-02-05 19:16:13 +0600

    file

--- src/component/forms/AddNewPackage.jsx
+++ src/component/forms/AddNewPackage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const AddNewPeopleForm = () => {
+const AddNewPackage = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const AddNewPeopleForm = () => {
   )
 }
 
-export default AddNewPeopleForm
+export default AddNewPackage
