b425731f83d6912d7f1d72038efd8e6035e1c341
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-02-26 01:14:34 +0600

    fix

--- src/app/(main)/register/page.jsx
+++ src/app/(main)/register/page.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const RegisterPage = () => {
   return (
     <div className='w-full flex items-center justify-center h-screen p-4'>
-      <div className='w-full sm:w-3/4 md:w-1/2 h-auto p-4 border-0 sm:border-2 rounded-xl border-black/10 shadow flex flex-col md:flex-row items-center justify-center '>
+      <div className='w-full sm:w-3/4 md:w-1/2 h-auto p-4 border-0 sm:border-2 rounded-xl border-black/10 shadow flex flex-col-reverse md:flex-row items-center justify-center '>
         <RegisterForm />
         <div className='flex-1 flex flex-col gap-1 items-center justify-center'>
           <p>Welcome to</p>
