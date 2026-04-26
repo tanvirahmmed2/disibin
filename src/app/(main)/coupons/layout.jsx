3984bc46fc617ef861d1f8d09ba9144db6e6ff19
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-04-26 22:05:12 +0600

    layout

---
+++ src/app/(main)/coupons/layout.jsx
@@ -0,0 +1,8 @@
+export const metadata = {
+    title: 'Save Money , Save Coupon',
+    description: 'Save money, use coupon for specialized development and design packages tailored to help your business grow.',
+}
+
+export default function PackagesLayout({ children }) {
+    return <section className="w-full">{children}</section>
+}
