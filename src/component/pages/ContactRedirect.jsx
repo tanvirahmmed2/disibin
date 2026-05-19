bfb185f15bd77068ce90d9a7c0a17d9037ccdc33
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-05-19 21:59:26 +0600

    contact

--- src/component/pages/ContactRedirect.jsx
+++ src/component/pages/ContactRedirect.jsx
@@ -1,14 +1,18 @@
+'use client'
+import { motion } from 'framer-motion'
 import Link from 'next/link'
 import React from 'react'
 
 const ContactRedirect = () => {
-  return (
-    <div className='w-full flex flex-col items-center justify-center gap-4 text-center p-4'>
-      <p className='text-4xl md:text-7xl bg-linear-to-br from-white to-slate-300 bg-clip-text'>Ready to build something great?</p>
-      <p>Tell us about your plan & we&apos;ll get back very soon!</p>
-      <Link href={'/contact'}>Write</Link>
-    </div>
-  )
+    return (
+        <div className='w-full flex flex-col items-center justify-center gap-4 text-center p-4'>
+            <motion.div  initial={{opacity:0, y:50}} whileInView={{opacity:1, y:0}} transition={{duration:0.6}} className='w-auto mx-auto gap-4 bg-black rounded-2xl p-4 flex flex-col items-center justify-center'>
+                <p className='text-4xl md:text-7xl bg-linear-to-br from-white p-4 font-poppins to-slate-300 bg-clip-text text-transparent'>Ready to build something great?</p>
+                <p className='text-white text-xl'>Tell us about your plan & we&apos;ll get back very soon!</p>
+                <Link href={'/contact'} className='bg-slate-100 px-6 p-2 rounded-2xl cursor-pointer font-jersey text-xl'>Contact</Link>
+            </motion.div>
+        </div>
+    )
 }
 
 export default ContactRedirect
